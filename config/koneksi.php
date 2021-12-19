de77954d127bc0921e10f815e5ba612e016ef3f7
Dwiki Nuwangsa <dwikinuwangsa@gmail.com>
2021-12-19 16:42:40 +0700

    main

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,14 +1,14 @@
 <?php 
-$link = mysqli_connect("localhost","byhkfabk_panel","MyFiLeArChIvE21","byhkfabk_panel");
-//$link = mysqli_connect("localhost","root","","mypanel");
+//$link = mysqli_connect("localhost","byhkfabk_panel","MyFiLeArChIvE21","byhkfabk_panel");
+$link = mysqli_connect("localhost","root","","mypanel");
 
 // Check connection
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
 }
 
-$conn = mysqli_connect("localhost","byhkfabk_dapodik_sync","MyFiLeArChIvE21","byhkfabk_dapodik_sync");
-//$conn = mysqli_connect("localhost","root","","dapodik_sync");
+//$conn = mysqli_connect("localhost","byhkfabk_dapodik_sync","MyFiLeArChIvE21","byhkfabk_dapodik_sync");
+$conn = mysqli_connect("localhost","root","","dapodik_sync");
 
 // Check connection
 if (mysqli_connect_errno()){
